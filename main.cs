29314dbe8def9fe449b47bad2315112277609416
ArthurTuggle <arthurtuggle57@yahoo.com>
2020-12-09 06:05:57 +0000

    errors

--- main.cs
+++ main.cs
@@ -2,16 +2,16 @@ using System;
 class HelloWorld {
 static void Main() {
   
-Console.WriteLine("Enter the name of item");
+Console.WriteLine("Enter name of item");
 string str;
 str = Console.ReadLine(); 
-Console.WriteLine("Enter the quantity");
+Console.WriteLine("Enter quantity");
 string a1;
 a1 = Console.ReadLine();
 int a2;
 a2 = Convert.ToInt32(a1);
   
-Console.WriteLine("Enter the price");
+Console.WriteLine("Enter price");
 string b1;
 b1 = Console.ReadLine();
 float b2 = 0.0f;
